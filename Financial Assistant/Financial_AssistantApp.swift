3323f67c8cc136bdeda0e110ee8d366469967a1d
Evgenii Goncharov <es.ydeni-s@yandex.ru>
2023-04-24 17:13:43 +0300

    Initial Commit

---
+++ Financial Assistant/Financial_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Financial_AssistantApp.swift
+//  Financial Assistant
+//
+//  Created by Евгений Гончаров on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Financial_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
